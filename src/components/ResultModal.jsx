a04a8a63a20a1ca2d6f99dd9ab745c10677664d9
Mayuri <mayurigurav1997@gmail.com>
2024-03-14 21:29:08 +0530

    Result is enhanced

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,12 @@
 import React, { forwardRef, useImperativeHandle, useRef } from 'react'
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
+
+    const userLost = remainingTime <= 0;
+    const formattedTime = (remainingTime / 1000).toFixed(2)
     const dialog = useRef()
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
     useImperativeHandle(ref, () => {
         return {
             open() {
@@ -11,10 +16,11 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
     });
     return (
         <dialog ref={dialog} className="result-modal" open>
-            <h2>You {result} </h2>
+            {userLost && <h2>You Lost </h2>}
+            {!userLost && <h2>You Score:{score} </h2>}
             <p>The Target time was <strong>{targetTime} seconds.</strong></p>
-            <p>You stopped the timer with <strong>X seconds left</strong></p>
-            <form action="" method="dialog">
+            <p>You stopped the timer with <strong>{formattedTime} seconds left</strong></p>
+            <form action="" method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
